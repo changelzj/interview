7b302fec4ef86af18caa9b76392591b7bc9c8a53
LiuZijian <lzj@123>
2020-03-30 20:58:53 +0800

    add

--- src/main/java/org/example/interview/runtime/TestRuntime.java
+++ src/main/java/org/example/interview/runtime/TestRuntime.java
@@ -1,10 +1,14 @@
 package org.example.interview.runtime;
 
+import sun.misc.VM;
+
 public class TestRuntime {
+    //-Xms1024m -Xmx1024m -XX:MaxDirectMemorySize=2g -XX:+PrintGCDetails 
     public static void main(String[] args) {
         Runtime runtime = Runtime.getRuntime();
         System.out.println("CPU:" + runtime.availableProcessors());
-        System.out.println("-Xmx JVM试图使用的最大堆内存" + runtime.maxMemory()/1024/1024);
-        System.out.println("-Xms JVM中的堆内存总量" + runtime.totalMemory()/1024/1024);
+        System.out.println("-Xmx JVM堆内存初始 " + runtime.maxMemory()/1024/1024);
+        System.out.println("-Xms JVM堆内存最大 " + runtime.totalMemory()/1024/1024);
+        System.out.println("JVM能用到的最大直接内存 " + VM.maxDirectMemory() /1024/1024);
     }
 }
